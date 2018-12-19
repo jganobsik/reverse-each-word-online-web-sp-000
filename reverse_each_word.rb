f4c416954d16e81bd1a6542d36d91a71dc8acfac
Joe Ganobsik <jganobsik@gmail.com>
2018-12-19 02:14:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,10 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  reversed_array = []
   array.collect do |word|
-    reversed= word.reverse!
-    reversed_array << reversed
+    word.reverse!
   end  
   
-  reversed_sentence = reversed_array.join(" ")
+  reversed_sentence = array.join(" ")
   
   return reversed_sentence
 end
